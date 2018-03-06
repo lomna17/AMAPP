0f65341151a205344f8989ec26004e7ac3adfd26
lomna17 <lomna17@gmail.com>
2018-03-05 23:29:04 -0500

    yeahboy

--- yeahboi/urls.py
+++ yeahboi/urls.py
@@ -14,12 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+#from django.urls import path
 from django.conf.urls import include
 from django.conf.urls import url
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path(r"", include("bloggyboi.urls")),
-    path('bloggyboi/', include('bloggyboi.urls'))
+    url('admin/', admin.site.urls),
+    url(r"", include("bloggyboi.urls")),
+    url('bloggyboi/', include('bloggyboi.urls'))
 ]
